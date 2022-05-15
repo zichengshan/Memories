344246e4892970241a33fa8ee78e4b9fa7db8654
Zicheng.S <zichengshan@gmail.com>
2022-05-14 23:21:02 -0700

    Set up routes

--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
 import express from 'express'
-import bodyParser from "body-parser";
-import mongoose from "mongoose";
+import bodyParser from "body-parser"
+import mongoose from "mongoose"
 import cors from 'cors'
+
+import postRoutes from "./routes/posts.js"
 
 const app = express()
 
@@ -9,9 +11,12 @@ app.use(bodyParser.json({limit: '30mb', extended: true}))
 app.use(bodyParser.urlencoded({limit: '30mb', extended: true}))
 app.use(cors());
 
+// Connect to the database
 const CONNECTION_URL = 'mongodb+srv://zichengS:1234567890@cluster0.dega9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 const PORT = process.env.PORT|| 4000
 
+app.use('/posts', postRoutes)
+
 mongoose.connect(CONNECTION_URL)
     .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
-    .catch((error) => console.log(`${error} did not connect`));
+    .catch((error) => console.log(`${error} did not connect`))
