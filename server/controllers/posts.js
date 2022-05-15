c882665ee00f1dadf781ddd0e36255d03e06554e
Zicheng.S <zichengshan@gmail.com>
2022-05-15 11:55:31 -0700

    controllers
    GET and POST request
    .find() and .save() is used

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,3 +1,28 @@
-export const getPosts = (req, res) => {
-    res.send('This Works!')
+import PostMessage from "../models/postMessage.js"
+
+export const getPosts = async (req, res) => {
+    try {
+        // .find() is used to find particular data from the MongoDB database
+        const postMessage = await PostMessage.find()
+        console.log(postMessage)
+
+        res.status(200).json(postMessage)
+    } catch (error) {
+        res.status(404).json({message: error.message})
+    }
+}
+
+export const createPost = async (req, res) => {
+    const post = req.body
+
+    const newPost = new PostMessage(post)
+
+    try {
+        // used to save the document to the database
+        await newPost.save()
+
+        res.status(201).json(newPost)
+    }catch (error){
+        res.status(409).json({message: error.message})
+    }
 }
