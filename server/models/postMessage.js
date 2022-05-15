85fb085cd686302ebefe2231c18884a8754797aa
Zicheng.S <zichengshan@gmail.com>
2022-05-15 11:34:50 -0700

    Backend folder structure
    1. postMessage.js used to enforce a specific schema at the application layer and provides an interface to the database

---
+++ server/models/postMessage.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose"
+
+//A Mongoose schema defines the structure of the document, default values, validators, etc.,
+// whereas a Mongoose model provides an interface to the database for creating, querying, updating, deleting records, etc.
+const postSchema = mongoose.Schema({
+    title: String,
+    message: String,
+    creator: String,
+    tags: [String],
+    selectedFile: String,
+    likeCount: {
+        type: Number,
+        default: 0,
+    },
+    createdAt: {
+        type: Date,
+        default: new Date(),
+    },
+})
+
+const PostMessage = mongoose.model('PostMessage', postSchema)
+
+export default PostMessage
