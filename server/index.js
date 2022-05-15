729dcf866f7df9ccc9d1a3e48ce02bdba8ced116
Zicheng.S <zichengshan@gmail.com>
2022-05-14 23:13:08 -0700

    Connecting to the MongoDB

--- server/index.js
+++ server/index.js
@@ -3,3 +3,15 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from 'cors'
 
+const app = express()
+
+app.use(bodyParser.json({limit: '30mb', extended: true}))
+app.use(bodyParser.urlencoded({limit: '30mb', extended: true}))
+app.use(cors());
+
+const CONNECTION_URL = 'mongodb+srv://zichengS:1234567890@cluster0.dega9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const PORT = process.env.PORT|| 4000
+
+mongoose.connect(CONNECTION_URL)
+    .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+    .catch((error) => console.log(`${error} did not connect`));
