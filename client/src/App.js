afb23dd9203d0057246a9c2c74f3065a40038ad5
Zicheng.S <zichengshan@gmail.com>
2022-05-15 17:21:37 -0700

    Redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
-import React from "react"
+import React, {useEffect} from "react"
 import {Container, AppBar, Typography, Grow, Grid} from "@material-ui/core"
+import {useDispatch} from "react-redux"
+import {getPosts} from './actions/posts'
 import memories from './images/memories.png'
 import Form from "./components/Form/Form"
 import Posts from './components/Posts/Posts'
@@ -7,6 +9,11 @@ import useStyles from './styles.js'
 
 function App() {
     const classes = useStyles()
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        dispatch(getPosts())
+    },[dispatch])
 
     return (
         <Container maxWidth="lg">
@@ -16,7 +23,7 @@ function App() {
             </AppBar>
             <Grow in>
                 <Container>
-                    <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+                    <Grid container justifyContent="space-between" alignItems="stretch" spacing={3}>
                         <Grid item xs={12} sm={7}>
                             <Posts/>
                         </Grid>
--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,9 +1,13 @@
 import React from "react";
 import Post from "./Post/Post";
 import useStyles from './styles'
+import {useSelector} from "react-redux"
+
 
 function Posts() {
+    const posts = useSelector((state) => state.posts)
     const classes = useStyles()
+    console.log(posts)
     return (
         <>
             <h1>POSTS</h1>
