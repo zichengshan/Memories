c5b26b5a2b738821d481b7d6ccdbd39195b22b9a
Zicheng.S <zichengshan@gmail.com>
2022-05-15 23:50:22 -0700

    Update Posts.js
    Add CircularProgress dynamic Icon

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -9,17 +9,16 @@ function Posts({setCurrentId}) {
     const posts = useSelector((state) => state.posts)
     const classes = useStyles()
     return (
-        // !posts.length ? <CircularProgress /> :
-        //     (
-            <Grid className={classes.mainContainer} container alignItems="stretch" spacing={3}>
-                {posts.map((post) => (
-                    <Grid key={post._id} item xs={12} sm ={6}>
-                        <Post post={post} setCurrentId={setCurrentId} />
-                    </Grid>
-                ))}
-            </Grid>
-        // )
-
+        !posts.length ? <CircularProgress/> :
+            (
+                <Grid className={classes.mainContainer} container alignItems="stretch" spacing={3}>
+                    {posts.map((post) => (
+                        <Grid key={post._id} item xs={12} sm={6}>
+                            <Post post={post} setCurrentId={setCurrentId}/>
+                        </Grid>
+                    ))}
+                </Grid>
+            )
     )
 }
 
