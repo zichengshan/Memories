a02f47d5df250e92d450c340d5953cd4800cf54c
Zicheng.S <zichengshan@gmail.com>
2022-05-16 22:15:05 -0700

    Environmental Variables

--- server/index.js
+++ server/index.js
@@ -2,8 +2,11 @@ import express from 'express'
 import bodyParser from "body-parser"
 import mongoose from "mongoose"
 import cors from 'cors'
+import dotenv from 'dotenv'
 
 import postRoutes from "./routes/posts.js"
+dotenv.config()
+
 
 const app = express()
 
@@ -12,11 +15,10 @@ app.use(bodyParser.urlencoded({limit: '30mb', extended: true}))
 app.use(cors());
 
 // Connect to the database
-const CONNECTION_URL = 'mongodb+srv://zichengS:1234567890@cluster0.dega9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 const PORT = process.env.PORT|| 4000
 
 app.use('/posts', postRoutes)
 
-mongoose.connect(CONNECTION_URL)
+mongoose.connect(process.env.CONNECTION_URL)
     .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
     .catch((error) => console.log(`${error} did not connect`))
