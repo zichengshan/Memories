7fe4b22ec73815465f11aaf555ca29fd69aae68d
Zicheng.S <zichengshan@gmail.com>
2022-05-23 21:50:08 -0700

    delete && update
    User can only delete and update the posts they created

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -18,13 +18,15 @@ function Post({post, setCurrentId}) {
         if (post.likes.length > 0) {
             return post.likes.find((like) => like === user ? user.result._id : user)
                 ? (
-                    <><ThumbUpAltIcon fontSize="small" />&nbsp;{post.likes.length > 2 ? `You and ${post.likes.length - 1} others` : `${post.likes.length} like${post.likes.length > 1 ? 's' : ''}` }</>
+                    <><ThumbUpAltIcon
+                        fontSize="small"/>&nbsp;{post.likes.length > 2 ? `You and ${post.likes.length - 1} others` : `${post.likes.length} like${post.likes.length > 1 ? 's' : ''}`}</>
                 ) : (
-                    <><ThumbUpAltOutlined fontSize="small" />&nbsp;{post.likes.length} {post.likes.length === 1 ? 'Like' : 'Likes'}</>
+                    <><ThumbUpAltOutlined
+                        fontSize="small"/>&nbsp;{post.likes.length} {post.likes.length === 1 ? 'Like' : 'Likes'}</>
                 );
         }
 
-        return <><ThumbUpAltOutlined fontSize="small" />&nbsp;Like</>;
+        return <><ThumbUpAltOutlined fontSize="small"/>&nbsp;Like</>;
     }
 
 
@@ -37,13 +39,18 @@ function Post({post, setCurrentId}) {
                 <Typography variant="h6">{post.name}</Typography>
                 <Typography variant="body2">{moment(post.createdAt).fromNow()}</Typography>
             </div>
-            <div className={classes.overlay2}>
-                <Button style={{color: 'white'}} size="small" onClick={() => setCurrentId(post._id)}>
-                    <MoreHorizIcon fontSize="default"/>
-                </Button>
-            </div>
+            
+            {user && post && (post.creator === user.result._id) && (
+                <div className={classes.overlay2}>
+                    <Button style={{color: 'white'}} size="small" onClick={() => setCurrentId(post._id)}>
+                        <MoreHorizIcon fontSize="default"/>
+                    </Button>
+                </div>
+            )}
+
             <div className={classes.details}>
-                <Typography variant="body2" color="textSecondary" component="h2">{post.tags.map((tag) => `#${tag} `)}</Typography>
+                <Typography variant="body2" color="textSecondary"
+                            component="h2">{post.tags.map((tag) => `#${tag} `)}</Typography>
             </div>
             <Typography className={classes.title} gutterBottom variant="h5" component="h2">{post.title}</Typography>
             <CardContent>
@@ -51,11 +58,14 @@ function Post({post, setCurrentId}) {
             </CardContent>
             <CardActions className={classes.cardActions}>
                 <Button size="small" color="primary" disabled={!user} onClick={() => dispatch(likePost(post._id))}>
-                    <Likes />
+                    <Likes/>
                 </Button>
-                <Button size="small" color="primary" onClick={() => dispatch(deletePost(post._id))}><DeleteIcon
-                    fontSize="small"/> Delete
-                </Button>
+                {user && post && (post.creator === user.result._id) && (
+                    <Button size="small" color="primary" onClick={() => dispatch(deletePost(post._id))}><DeleteIcon
+                        fontSize="small"/> Delete
+                    </Button>
+                )}
+
             </CardActions>
         </Card>
     );
